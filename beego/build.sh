5721c21c1afbea6bf0ed3aa8269a7ec8d32d7b7b
extralam <certain603@gmail.com>
2015-09-14 00:24:13 +0800

    Update build.sh

--- beego/build.sh
+++ beego/build.sh
@@ -1,10 +1,5 @@
 #/bin/bash 
 
-ADD github.com/beego/bee
-ADD github.com/astaxie/beego
-ADD github.com/go-sql-driver/mysql
-ADD github.com/howeyc/fsnotify
-ADD github.com/lib/pq
-ADD github.com/smartystreets/goconvey/convey
-ADD github.com/jtolds/gls
-go install 
+cd /go/src/github.com/beego/bee
+
+go install
